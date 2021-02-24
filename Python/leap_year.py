fae399ee3d8f12d7cef38521e8e545aa9831db2d
CodingAnUtopy <79327727+CodingAnUtopy@users.noreply.github.com>
2021-02-24 11:14:43 +0100

    Add files via upload

--- Python/leap_year.py
+++ Python/leap_year.py
@@ -3,16 +3,12 @@
 # URL: ./231174-creez-des-structures-conditionnelles
 
 def isleapyear(year):
-    if year % 4 != 0:
-        return 0  # Not a leap year
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return 1
-            else:
-                return 0
-        else:
-            return 1
+    if year <= 0:
+        raise ValueError()
+    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+        return 1
+    else:
+        return 0
 
 
 # Test
