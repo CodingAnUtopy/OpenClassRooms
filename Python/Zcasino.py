d465e91f5d8528482479292b7c478c88c865558d
CodingAnUtopy <79327727+CodingAnUtopy@users.noreply.github.com>
2021-02-25 20:14:42 +0100

    Update Zcasino.py

--- Python/Zcasino.py
+++ Python/Zcasino.py
@@ -1,67 +1,23 @@
-# Python exercise: ZCasino
+# Python exercise: determining if the input is a leap year
 # MOOC: https://openclassrooms.com/fr/courses/235344-apprenez-a-programmer-en-python/
-# URL: ./231735-tp-tous-au-zcasino
+# URL: ./231174-creez-des-structures-conditionnelles
 
-# To do: integer input checker + loop until valid input value
-
-from random import randrange
-from math import ceil
-
-
-def printgui(wallet, bet, number, random, result):
-    print("You bet", bet, "on the number", number, ".")
-    print("The random number is", random, ".")
-    # Printing results
-    if result == -1:
-        print("You lost your bet.")
-    elif result == 0:
-        print("You get back 50% of your bet.")
-    elif result == 1:
-        print("Congratulations! You won!")
-    print("You have now", wallet, "in your wallet.")
-    print()
-
-
-def getgain(wallet, bet, number, random):
-    result = -1  # -1 = lose bet, 0 = get back 50%, 1 = win 300% bet
-    if number == random:
-        result = 1
-        return result, wallet + (2 * bet)
-    elif number % 2 == random % 2:
-        result = 0
-        return result, wallet - bet + ceil(bet / 2)
+def isleapyear(year):
+    if year <= 0:
+        raise ValueError()
+    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+        return 1
     else:
-        return result, wallet - bet
-
-
-def playzcasino(wallet):
-    try:
-        # Generating a random number
-        random = randrange(50)
-        # Play a number and place a bet
-        number = int(input("Play a number between 0 and 49: "))
-        assert 0 <= number <= 49  # range of numbers is [minimum;maximum] = [0;49]
-        bet = int(input("Amount to bet: "))
-        assert 0 < bet <= wallet
-        # Playing the game
-        result, wallet = getgain(wallet, bet, number, random)
-        # Printing GUI
-        printgui(wallet, bet, number, random, result)
-        # Updating wallet
-        return wallet
-    except AssertionError:
-        print("Number not in range.")
-    except:
-        print("Something went wrong.")
+        return 0
 
 
 # Test
 if __name__ == "__main__":
-    playerWallet = 200
-    playAgain = 1
-    while playerWallet > 0 and playAgain != 0:
-        playerWallet = playzcasino(playerWallet)
-        if playerWallet > 0:
-            playAgain = int(input("Keep playing? (1=yes 0=no)"))
+    try:
+        userInput = int(input("Enter a year: "))
+        if isleapyear(userInput):
+            print("It is a leap year.")
         else:
-            print("GAME OVER! No money, no gamble.")
+            print("It is not a leap year.")
+    except:
+        print("Something went wrong.")
