dea2effe48785db317b5a061c0a7d1b22992087a
CodingAnUtopy <79327727+CodingAnUtopy@users.noreply.github.com>
2021-02-27 15:22:26 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# Configuration used in the game hangman.py
+
+MAXIMUM_LETTERS_PER_WORD = 5
+DICTIONARY_PATH = "C:/Users/kxaya/PycharmProjects/OCR-Python/hangman/dictionary.txt"
+SCORES_PATH = "C:/Users/kxaya/PycharmProjects/OCR-Python/hangman/scores"
+MAXIMUM_TURNS_PER_ROUND = 8
