07ef0b8e96cc93b5b2a2d1e13b9286e0ec32a071
CodingAnUtopy <79327727+CodingAnUtopy@users.noreply.github.com>
2021-02-28 09:22:32 +0100

    Update config.py

--- hangman/config.py
+++ hangman/config.py
@@ -1,6 +1,6 @@
 # Configuration used in the game hangman.py
 
-MAXIMUM_LETTERS_PER_WORD = 5
-DICTIONARY_PATH = "C:/Users/kxaya/PycharmProjects/OCR-Python/hangman/dictionary.txt"
-SCORES_PATH = "C:/Users/kxaya/PycharmProjects/OCR-Python/hangman/scores"
-MAXIMUM_TURNS_PER_ROUND = 8
+MAXIMUM_LETTERS_PER_WORD = 5  # Dictionary can contain words with different length, only words with desired maximum letters will be selected
+DICTIONARY_PATH = "<Enter absolute path to dictionary file>"
+SCORES_PATH = "<Enter absolute path to scores file>"
+MAXIMUM_TURNS_PER_ROUND = 8  # Maximum number of attempts to find the mystery word
